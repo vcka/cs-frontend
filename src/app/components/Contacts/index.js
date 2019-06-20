6f37127762ee4bb9d6c4c01eb5a5359309d55cf7
vcka <vycioxas@gmail.com>
2019-06-20 21:28:25 +0300

    Hookssss

--- src/app/components/Contacts/index.js
+++ src/app/components/Contacts/index.js
@@ -1,21 +1,41 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import { useFetch } from "../../utils/hooks";
+import { API_ENDPOINTS } from "../../utils/constants";
+import "./index.css";
+// import * as icons from "react-icons/fa";
+import {
+  FaEnvelope as EMAIL,
+  FaPhone as PHONE,
+  FaMapMarker as LOCATION,
+  FaLinkedinIn as LINKEDIN,
+  FaGithubAlt as GIT
+} from "react-icons/fa";
 
-function Item(data){
-    const Icon = data.icon;
-    return(
-        <a className="Contacts--item" href={data.link}>
-            <Icon className="Contacts--item-icon" />
-            <span className="Contacts--item-text">{data.text}</span>
-        </a>  
-    );
+const icons = { EMAIL, PHONE, LOCATION, LINKEDIN, GIT };
+
+function Item(data) {
+  const Icon = icons[data.contactsType];
+  return (
+    <a className="Contacts--item" href={data.link}>
+      <Icon className="Contacts--item-icon" />
+      <span className="Contacts--item-text">{data.value}</span>
+    </a>
+  );
 }
 
-function Contacts(props){
-    return (<section className="Contacts">
+function Contacts() {
+  const { loading, data } = useFetch(API_ENDPOINTS.personContacts);
+  if (loading) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <section className="Contacts">
         <h3>Contacts</h3>
-        {props.items.map(Item)}
-    </section>);
+        {console.log(data)}
+        {data.map(Item)}
+      </section>
+    );
+  }
 }
 
 export default Contacts;
--- src/app/utils/constants.js
+++ src/app/utils/constants.js
@@ -1,6 +1,7 @@
 export const API_DOMAIN = "http://localhost:8089";
 export const API_ENDPOINTS = {
   personInfo: "/api/person/info/",
-  personSkills: "/api/person/skills"
+  personSkills: "/api/person/skills",
+  personContacts: "/api/contacts/show"
   // ir t.t.
 };
